044b4207c33917615919421ac5208e798dfdf653
kimkyungsub <cyon256@icloud.com>
2024-08-05 10:26:39 +0900

    build author schema

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
 	implementation("org.postgresql:postgresql")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     // Use JUnit Jupiter for testing.
--- app/src/main/java/org/example/models/Author.java
+++ app/src/main/java/org/example/models/Author.java
@@ -1,10 +1,48 @@
 package org.example.models;
 
-/**
- * Author
- */
-public class Author {
-	
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-	
+
+/**
+ * The Author class represents an author with personal details.
+ * It includes properties for storing the author's ID, first name, last name, email, and age.
+ */
+@Entity
+@Data
+@NoArgsConstructor
+public class Author {
+
+    /**
+     * The unique identifier for the author.
+     */
+	@Id
+	@GeneratedValue(
+		strategy = GenerationType.IDENTITY
+	)
+    private Integer id;
+
+    /**
+     * The first name of the author.
+     */
+    private String firstName;
+
+    /**
+     * The last name of the author.
+     */
+    private String lastName;
+
+    /**
+     * The email address of the author.
+     */
+    private String email;
+
+    /**
+     * The age of the author.
+     */
+    private int age;
 }
