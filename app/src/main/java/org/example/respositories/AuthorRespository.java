6b9f7ddcb641dd02e16b1b4a0422f0afe37adb32
kimkyungsub <cyon256@icloud.com>
2024-08-06 18:27:45 +0900

    creating queries

--- app/src/main/java/org/example/respositories/AuthorRespository.java
+++ app/src/main/java/org/example/respositories/AuthorRespository.java
@@ -1,12 +1,19 @@
 package org.example.respositories;
 
+import java.util.List;
+
 import org.example.models.AuthorModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 /**
  * Author
  */
 public interface AuthorRespository extends JpaRepository<AuthorModel, Integer> {
-
-	
+	// select * from author where fist_name = $1;
+	List<AuthorModel> findAllByFirstName(String firstName);
+	// ignore case
+	List<AuthorModel> findAllByFirstNameIgnoreCase(String firstName);
+	// select * from author where fist_name like = '%$1%';
+	List<AuthorModel> findAllByFirstNameContainingIgnoreCase(String firstName);
+	//
+	List<AuthorModel> findAllByFirstNameStartsWithIgnoreCase(String firstName);
 }
